ca6950ae2b8c74bd13570c9662e514bf337ed311
Kaarthikeyan Raghavan <kaarthik.raghavan@gmail.com>
2019-09-22 12:28:18 +0530

    Add project files.

---
+++ SampleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+			Console.WriteLine("Kaarthikeyan");
+			Console.Read();
+		}
+	}
+}
